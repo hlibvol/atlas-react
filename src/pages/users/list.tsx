39b723c263a808056ca8cfae5e25907af7fd9d1e
Shas Legacy <sojha@atlasbeacon.com>
2022-09-20 01:19:36 +0530

    remove unwanted files, bug and packages

--- src/pages/users/list.tsx
+++ src/pages/users/list.tsx
@@ -18,6 +18,9 @@ import {
   Typography,
   BooleanField,
   TextField,
+  EditButton,
+  DeleteButton,
+  ShowButton,
 } from "@pankod/refine-antd";
 
 import { IUser, IRole } from "interfaces";
@@ -38,65 +41,6 @@ export const UsersList: React.FC<IResourceComponentsProps> = () => {
   const { data: roles, isLoading } = useList<IRole>({
     resource: "roles",
   });
-
-  const { mutate: mutateDelete } = useDelete();
-
-  const moreMenu = (id: number) => (
-    <Menu
-      mode="vertical"
-      onClick={({ domEvent }) => domEvent.stopPropagation()}
-    >
-      <Menu.Item
-        key="accept"
-        style={{
-          fontSize: 15,
-          display: "flex",
-          alignItems: "center",
-          fontWeight: 500,
-        }}
-        icon={
-          <Icons.EditOutlined
-            style={{
-              color: "#52c41a",
-              fontSize: 17,
-              fontWeight: 500,
-            }}
-          />
-        }
-        onClick={() => {
-          edit("users", id);
-        }}
-      >
-        {t("buttons.edit")}
-      </Menu.Item>
-      <Menu.Item
-        key="reject"
-        style={{
-          fontSize: 15,
-          display: "flex",
-          alignItems: "center",
-          fontWeight: 500,
-        }}
-        icon={
-          <Icons.CloseCircleOutlined
-            style={{
-              color: "#EE2A1E",
-              fontSize: 17,
-            }}
-          />
-        }
-        onClick={() => {
-          mutateDelete({
-            resource: "users",
-            id,
-            mutationMode: "undoable",
-          });
-        }}
-      >
-        {t("buttons.delete")}
-      </Menu.Item>
-    </Menu>
-  );
 
   return (
     <List>
@@ -123,7 +67,6 @@ export const UsersList: React.FC<IResourceComponentsProps> = () => {
             </Space>
           )}
         />
-        <Table.Column dataIndex="role_id" title={t("users.fields.role")} />
 
         <Table.Column
           dataIndex={["role_id"]}
@@ -160,19 +103,14 @@ export const UsersList: React.FC<IResourceComponentsProps> = () => {
           render={(value) => <BooleanField value={value} />}
         />
         <Table.Column<IUser>
-          fixed="right"
           title={t("table.actions")}
           dataIndex="actions"
-          key="actions"
           render={(_, record) => (
-            <Dropdown overlay={moreMenu(record.id)} trigger={["click"]}>
-              <Icons.MoreOutlined
-                onClick={(e) => e.stopPropagation()}
-                style={{
-                  fontSize: 24,
-                }}
-              />
-            </Dropdown>
+            <Space>
+              <EditButton hideText size="small" recordItemId={record.id} />
+              <ShowButton hideText size="small" recordItemId={record.id} />
+              <DeleteButton hideText size="small" recordItemId={record.id} />
+            </Space>
           )}
         />
       </Table>
